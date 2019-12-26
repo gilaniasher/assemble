f9ddfc60e668260933561b8428e208960c66590e
Asher <gilaniasher@gmail.com>
2019-12-26 18:05:38 -0500

    Replaced endpoints with EC2 database credentials. Still not working

--- AWSLambdaNodeFunctions/CreateUser/index.js
+++ AWSLambdaNodeFunctions/CreateUser/index.js
@@ -1,9 +1,9 @@
 const neo4j = require('neo4j-driver');
-const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'assemble'));
+const driver = neo4j.driver('bolt://assemble-neo4j.ml:7687', neo4j.auth.basic('neo4j', 'i-04d98b05c5ff39997assemble'));
 const session = driver.session();
 
 exports.handler = async (event) => {
-    var univ;
+    let univ;
 
     session.run('MATCH (n:User {name: \'Asher Gilani\'}) RETURN n')
     .then(result => {
