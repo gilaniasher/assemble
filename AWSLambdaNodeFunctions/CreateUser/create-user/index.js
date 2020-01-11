39eb9d5114821d0d00c9985ddf4b7328c8e54ce3
Asher Gilani <gilaniasher@gmail.com>
2020-01-11 01:00:04 -0500

    Filled in CreateUser function. SamLocalResource keeps timing out when running locally though

--- AWSLambdaNodeFunctions/CreateUser/create-user/index.js
+++ AWSLambdaNodeFunctions/CreateUser/create-user/index.js
@@ -1,6 +1,25 @@
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver('bolt://assemble-neo4j.ml', neo4j.auth.basic('neo4j', 'i-04d98b05c5ff39997'));
+const session = driver.session();
+
 exports.handler = async (event, context) => {
-    return {
-        'statusCode': 200,
-        'body': 'OK'
-    }
+    return session
+        .run(`CREATE (n:User {name: 'Lambda Test'})`)
+        .then(() => {
+            session.close();
+            driver.close();
+
+            return {
+                'statusCode': 200
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            session.close();
+            driver.close();
+
+            return {
+                'statusCode': 400
+            }
+        })
 };
