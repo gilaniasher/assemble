5fa4cb2e41b92d0648cade2f62b2783c6873b59b
Asher <gilaniasher@gmail.com>
2020-03-23 02:38:09 -0400

    Added icons to the tab navigator; Would still be nice if tab text only appeared if you were on that screen

--- ReactNativeApp/src/components/TabNavigator.js
+++ ReactNativeApp/src/components/TabNavigator.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import HomeScreen from '../screens/Home';
 import SettingsScreen from '../screens/Signup';
+
+const routeIcons = ({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+        }
+
+        return <Icon name={iconName} size={25} color={color} />;
+    }
+});
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function MyTabs() {
   return (
     <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+            initialRouteName="Home"
+            barStyle={{ backgroundColor: '#592E83'}}
+            screenOptions={routeIcons}
+        >
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
         </Tab.Navigator>
